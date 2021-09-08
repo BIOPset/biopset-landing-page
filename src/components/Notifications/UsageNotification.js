bec8ce00e432e50f4f8f54ff9bc230ca4c0292b7
munair <munair@gmail.com>
2021-09-08 12:22:27 +0800

    seems a bit better

--- src/components/Notifications/UsageNotification.js
+++ src/components/Notifications/UsageNotification.js
@@ -8,22 +8,23 @@ import { Row, Container } from "reactstrap";
 function UsageNotification() {
   return (
     
-    <div id="usagenotification" className="section text-center">
-      <div class="alert-with-icon alert alert-danger fade show" role="alert">
-        <div class="container">
-          <div class="alert-wrapper">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-              <i class="nc-icon nc-simple-remove" />
-            </button>
-            <div class="message">
-              <i class="nc-icon nc-bell-55" /> 
-              <h6>DO NOT USE BIOPSET IN REGULATED JURISDICTIONS.</h6>
+    <div id="usagenotification">
+      <div className="section text-center">
+        <div class="alert-with-icon alert alert-danger fade show" role="alert">
+          <div class="container">
+            <div class="alert-wrapper">
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <i class="nc-icon nc-simple-remove" />
+              </button>
+              <div class="message">
+                <i class="nc-icon nc-bell-55" /> 
+                <h6>DO NOT USE BIOPSET IN REGULATED JURISDICTIONS.</h6>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  
   );
 }
 
