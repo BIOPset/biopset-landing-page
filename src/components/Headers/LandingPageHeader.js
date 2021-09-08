cb45989e9b6567ce0672bd72e0e24ac2ee651091
munair <munair@gmail.com>
2021-09-08 12:00:44 +0800

    reduce scope a tad so that we remove the parallax effect.

--- src/components/Headers/LandingPageHeader.js
+++ src/components/Headers/LandingPageHeader.js
@@ -83,17 +83,17 @@ function LandingPageHeader() {
             </Button>
           </div>
         </Container>
-      </div>
-      <div id="notifications">
-        <div class="alert-with-icon alert alert-danger fade show" role="alert">
-          <div class="container">
-            <div class="alert-wrapper">
-              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <i class="nc-icon nc-simple-remove" />
-              </button>
-              <div class="message">
-                <i class="nc-icon nc-bell-55" /> 
-                <h6>DO NOT USE BIOPSET IN REGULATED JURISDICTIONS.</h6>
+        <div id="notifications">
+          <div class="alert-with-icon alert alert-danger fade show" role="alert">
+            <div class="container">
+              <div class="alert-wrapper">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <i class="nc-icon nc-simple-remove" />
+                </button>
+                <div class="message">
+                  <i class="nc-icon nc-bell-55" /> 
+                  <h6>DO NOT USE BIOPSET IN REGULATED JURISDICTIONS.</h6>
+                </div>
               </div>
             </div>
           </div>
