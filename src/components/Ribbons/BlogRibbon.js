bdb7e40a62597708566db87ace1358511e1e1e00
munair <munair@gmail.com>
2021-09-08 14:03:23 +0800

    reimplement brandIcon

--- src/components/Ribbons/BlogRibbon.js
+++ src/components/Ribbons/BlogRibbon.js
@@ -2,6 +2,9 @@
 /*eslint-disable*/
 import React from "react";
 
+// brand icon
+import brandIcon from '../assets/img/biop-brand-icon-128x128px.png';
+
 // reactstrap components
 import { Row, Container } from "reactstrap";
 
