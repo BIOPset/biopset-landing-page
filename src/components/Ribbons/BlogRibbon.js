38f90a6dbc1b9b70b76f4cfd61e1bc9f96bbaa9c
munair <munair@gmail.com>
2021-09-08 14:04:26 +0800

    renest image url

--- src/components/Ribbons/BlogRibbon.js
+++ src/components/Ribbons/BlogRibbon.js
@@ -3,7 +3,7 @@
 import React from "react";
 
 // brand icon
-import brandIcon from '../assets/img/biop-brand-icon-128x128px.png';
+import brandIcon from '../../assets/img/biop-brand-icon-128x128px.png';
 
 // reactstrap components
 import { Row, Container } from "reactstrap";
