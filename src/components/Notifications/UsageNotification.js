7922ba8cfdc6dd45395822258a6b4d635a4f700b
munair <munair@gmail.com>
2021-09-08 12:24:45 +0800

    try to get notification on one line

--- src/components/Notifications/UsageNotification.js
+++ src/components/Notifications/UsageNotification.js
@@ -17,8 +17,7 @@ function UsageNotification() {
                 <i class="nc-icon nc-simple-remove" />
               </button>
               <div class="message">
-                <i class="nc-icon nc-bell-55" /> 
-                <h6>DO NOT USE BIOPSET IN REGULATED JURISDICTIONS.</h6>
+                <h6><i class="nc-icon nc-bell-55" /> PLEASE DO NOT USE BIOPSET IN REGULATED JURISDICTIONS.</h6>
               </div>
             </div>
           </div>
