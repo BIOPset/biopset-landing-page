dd64746aed97f5f577aadc50b45ef1acff95ff93
munair <munair@gmail.com>
2021-09-08 11:56:23 +0800

    relocate notification to landing page header

--- src/components/Headers/LandingPageHeader.js
+++ src/components/Headers/LandingPageHeader.js
@@ -82,6 +82,21 @@ function LandingPageHeader() {
               Learn
             </Button>
           </div>
+          <div id="notifications">
+            <div class="alert-with-icon alert alert-danger fade show" role="alert">
+              <div class="container">
+                <div class="alert-wrapper">
+                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <i class="nc-icon nc-simple-remove" />
+                  </button>
+                  <div class="message">
+                    <i class="nc-icon nc-bell-55" /> 
+                    <h6>DO NOT USE BIOPSET IN REGULATED JURISDICTIONS.</h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </Container>
       </div>
     </>
