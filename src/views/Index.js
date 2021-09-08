aba3aaeeace0f8cd431c068d7ba39bd8f60b24bf
munair <munair@gmail.com>
2021-09-08 11:16:38 +0800

    remove link

--- src/views/Index.js
+++ src/views/Index.js
@@ -69,7 +69,6 @@ function LandingPage() {
                       Buy Options
                     </Button>
                     <h6 className="info-title">Double Your Wealth</h6>
-                    <a href="https://docs.biopset.com/theory/fundamentals/buying-options"><i className="fa fa-info-circle" /></a>
                     <h6 className="description">
                       Make up to 2X returns on the premiums paid whenever you correctly predict the direction
                       of ETH prices between oracle updates.
