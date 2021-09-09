8812e6a8337da31f5dc80cff51029d029a63fde8
munair <munair@gmail.com>
2021-09-10 02:46:10 +0800

    testing

--- src/views/index-sections/Aspiration.js
+++ src/views/index-sections/Aspiration.js
@@ -15,6 +15,37 @@ function Aspiration() {
     setModal(!modal);
   };
 
+  const [loaded, setLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    const scriptTag = document.createElement('script');
+    scriptTag.src = "https://medium-widget.pixelpoint.io/widget.js";
+    scriptTag.addEventListener('load', () => setLoaded(true));
+    document.body.appendChild(scriptTag);
+  }, []);
+
+  React.useEffect(() => {
+    if (!loaded) return;
+    // the <script src="https://medium-widget.pixelpoint.io/widget.js"></script> should be loaded.
+
+    window.MediumWidget.Init({
+        renderTo: "#medium-widget",
+        params: {
+          resource: "https://medium.com/biopset",
+          postsPerLine: 1,
+          limit: 2,
+          picture: "big",
+          fields: ["description", "author", "claps", "publishAt"],
+          ratio: "landscape"
+        }
+    })
+
+    return () => {
+      window.MediumWidget.unmount();
+    };
+  
+  }, [loaded])
+
   return (
     <div 
       className="section text-center" 
@@ -86,10 +117,10 @@ function Aspiration() {
                       className="modal-title text-center"
                       id="exampleModalLabel"
                     >
-                      Modal title
+                      Recent Articles
                     </h5>
                   </div>
-                  <div className="modal-body">
+                  <div className="modal-body ml-auto mr-auto" xs="10">
                     <div id="medium-widget" />
                   </div>
                   <div className="modal-footer">
