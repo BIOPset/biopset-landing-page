1780f954929d0b0e4a819936094ad2bd8dc46cde
munair <munair@gmail.com>
2021-09-10 02:34:19 +0800

    add modal

--- src/views/index-sections/Aspiration.js
+++ src/views/index-sections/Aspiration.js
@@ -10,6 +10,10 @@ import brandIcon from '../../assets/img/biop-white-icon-128x128px.png';
 // core components
 
 function Aspiration() {
+  const [modal, setModal] = React.useState(false);
+  const toggleModal = () => {
+    setModal(!modal);
+  };
 
   return (
     <div 
@@ -62,11 +66,61 @@ function Aspiration() {
                   href="#blog"
                   className="btn-round mr-4"
                   color="danger"
-                  onClick={(e) => e.preventDefault()}
+                  onClick={toggleModal}
                 >
                   <i className="fa fa-search" />
                   Research Blog
                 </Button>
+                {/* Modal */}
+                <Modal isOpen={modal} toggle={toggleModal}>
+                  <div className="modal-header">
+                    <button
+                      aria-label="Close"
+                      className="close"
+                      type="button"
+                      onClick={toggleModal}
+                    >
+                      <span aria-hidden={true}>×</span>
+                    </button>
+                    <h5
+                      className="modal-title text-center"
+                      id="exampleModalLabel"
+                    >
+                      Modal title
+                    </h5>
+                  </div>
+                  <div className="modal-body">
+                    Far far away, behind the word mountains, far from the
+                    countries Vokalia and Consonantia, there live the blind texts.
+                    Separated they live in Bookmarksgrove right at the coast of
+                    the Semantics, a large language ocean. A small river named
+                    Duden flows by their place and supplies it with the necessary
+                    regelialia. It is a paradisematic country, in which roasted
+                    parts of sentences fly into your mouth. Even the all-powerful
+                    Pointing has no control about the blind texts it is an almost
+                    unorthographic life One day however a small line of blind text
+                    by the name of Lorem Ipsum decided to leave for the far World
+                    of Grammar.
+                  </div>
+                  <div className="modal-footer">
+                    <div className="left-side">
+                      <Button
+                        className="btn-link"
+                        color="default"
+                        type="button"
+                        onClick={toggleModal}
+                      >
+                        Never mind
+                      </Button>
+                    </div>
+                    <div className="divider" />
+                    <div className="right-side">
+                      <Button className="btn-link" color="danger" type="button">
+                        Delete
+                      </Button>
+                    </div>
+                  </div>
+                </Modal>
                 <Button
                   href="#video"
                   className="btn-round mr-4"
