cfe61d83c2bb7a1db95f21f60eb9ce1fdb889012
munair <munair@gmail.com>
2021-09-09 23:10:32 +0800

    single quotes

--- src/views/index-sections/Aspiration.js
+++ src/views/index-sections/Aspiration.js
@@ -26,10 +26,10 @@ function Aspiration() {
             <img src={brandIcon} width="10%" alt="brand icon" />
             <h3 className="title">The Aspiration</h3>
             <h5 className="description" style={{fontFamily: "octarine-bold", textTransform: "lowercase"}}>
-              The binary options settlement protocol ("<span style="color: red">BIOPSET</span>") aims to be the most transparent programmatic 
-              clearinghouse for buying and selling binary options and esure all users ("<span style="color: red">settlers</span>") may leverage the 
+              The binary options settlement protocol ("<span style='color: red'>BIOPSET</span>") aims to be the most transparent programmatic 
+              clearinghouse for buying and selling binary options and esure all users ("<span style='color: red'>settlers</span>") may leverage the 
               protocol to earn fees for settling on-demand binary options offered by liquidity providers 
-              ("<span style="color: red">writers</span>") seeking to earn premiums from price speculators ("<span style="color: red">traders</span>"). 
+              ("<span style='color: red'>writers</span>") seeking to earn premiums from price speculators ("<span style='color: red'>traders</span>"). 
             </h5>
           </Col>
         </Row>
