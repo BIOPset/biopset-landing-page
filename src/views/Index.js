09fa4d030c8f1af42b293a5f430b9c45773ec043
munair <munair@gmail.com>
2021-09-09 13:51:00 +0800

    simplify newslettr

--- src/views/Index.js
+++ src/views/Index.js
@@ -384,20 +384,9 @@ function LandingPage() {
           <Container>
             <Row>
               <Col className="ml-auto mr-auto" md="8">
-                <h2 className="text-center">Keep in touch?</h2>
+                <h2 className="text-center">Latest Developments</h2>
                 <Form className="contact-form">
                   <Row>
-                    <Col md="6">
-                      <label>Name</label>
-                      <InputGroup>
-                        <InputGroupAddon addonType="prepend">
-                          <InputGroupText>
-                            <i className="nc-icon nc-single-02" />
-                          </InputGroupText>
-                        </InputGroupAddon>
-                        <Input placeholder="Name" type="text" />
-                      </InputGroup>
-                    </Col>
                     <Col md="6">
                       <label>Email</label>
                       <InputGroup>
@@ -409,17 +398,9 @@ function LandingPage() {
                         <Input placeholder="Email" type="text" />
                       </InputGroup>
                     </Col>
-                  </Row>
-                  <label>Message</label>
-                  <Input
-                    placeholder="Tell us your thoughts and feelings..."
-                    type="textarea"
-                    rows="4"
-                  />
-                  <Row>
                     <Col className="ml-auto mr-auto" md="4">
                       <Button className="btn-fill" color="danger" size="lg">
-                        Send Message
+                        Subscribe
                       </Button>
                     </Col>
                   </Row>
