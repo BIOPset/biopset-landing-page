2f542348c9f8b8818f048c151785502be35cb3e0
munair <munair@gmail.com>
2021-09-11 13:57:25 +0900

    add back height of 100%

--- src/views/index-sections/Blog.js
+++ src/views/index-sections/Blog.js
@@ -133,11 +133,11 @@ function Blog() {
                 Take a look at this video if you are new to binary options and want to 
                 learn how BIOPset can be a benefit to you.
               </p>
-              <iframe src="https://www.youtube.com/embed/G86wkheYmdc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+              <iframe height="100%" src="https://www.youtube.com/embed/G86wkheYmdc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </TabPane>
             <TabPane tabId="3">
               <p>The beating heart of BIOPset is our community. Please join us in Discord.</p>
-              <iframe src="https://discord.com/widget?id=795000201742909450&theme=dark" title="BIOPset Discord Widget" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+              <iframe height="100%" src="https://discord.com/widget?id=795000201742909450&theme=dark" title="BIOPset Discord Widget" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
             </TabPane>
           </TabContent>
         </Col>
