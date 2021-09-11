b192b9f244d73e4eb9f08653a5db67c9bdfc31d2
munair <munair@gmail.com>
2021-09-11 19:32:08 +0900

    update founder profiles

--- src/views/index-sections/Founders.js
+++ src/views/index-sections/Founders.js
@@ -20,26 +20,34 @@ function Founders() {
   return (
     <div className="section text-center">
       <Container>
-        <h2 className="title">Founders</h2>
+      <Container>
         <Row>
-          <Col md="4">
+          <Col className="ml-auto mr-auto" md="10">
+            <div className="title">
+              <h2>Founders</h2>
+              <br />
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col md="3">
             <Card className="card-profile card-plain">
               <div className="card-avatar">
-                <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                <a href="#shalaquiana" onClick={(e) => e.preventDefault()}>
                   <img
-                    alt="..."
+                    alt="shalaquiana profile image"
                     src={
-                      require("assets/img/faces/clem-onojeghuo-3.jpg")
+                      require("assets/img/faces/shalaquiana-profile-image.jpg")
                         .default
                     }
                   />
                 </a>
               </div>
               <CardBody>
-                <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                <a href="#shalaquiana" onClick={(e) => e.preventDefault()}>
                   <div className="author">
-                    <CardTitle tag="h4">Henry Ford</CardTitle>
-                    <h6 className="card-category">Product Manager</h6>
+                    <CardTitle tag="h4">Shalaquiana</CardTitle>
+                    <h6 className="card-category">Core Developer</h6>
                   </div>
                 </a>
                 <p className="card-description text-center">
@@ -52,44 +60,28 @@ function Founders() {
                 <Button
                   className="btn-just-icon btn-neutral"
                   color="link"
-                  href="#pablo"
+                  href="https://twitter.com/shalaquiana"
                   onClick={(e) => e.preventDefault()}
                 >
                   <i className="fa fa-twitter" />
                 </Button>
-                <Button
-                  className="btn-just-icon btn-neutral ml-1"
-                  color="link"
-                  href="#pablo"
-                  onClick={(e) => e.preventDefault()}
-                >
-                  <i className="fa fa-google-plus" />
-                </Button>
-                <Button
-                  className="btn-just-icon btn-neutral ml-1"
-                  color="link"
-                  href="#pablo"
-                  onClick={(e) => e.preventDefault()}
-                >
-                  <i className="fa fa-linkedin" />
-                </Button>
               </CardFooter>
             </Card>
           </Col>
-          <Col md="4">
+          <Col md="3">
             <Card className="card-profile card-plain">
               <div className="card-avatar">
-                <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                <a href="#zero" onClick={(e) => e.preventDefault()}>
                   <img
-                    alt="..."
+                    alt="zero profile image"
                     src={
-                      require("assets/img/faces/joe-gardner-2.jpg").default
+                      require("assets/img/faces/zero-profile-image.jpg").default
                     }
                   />
                 </a>
               </div>
               <CardBody>
-                <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                <a href="#dereek69" onClick={(e) => e.preventDefault()}>
                   <div className="author">
                     <CardTitle tag="h4">Sophie West</CardTitle>
                     <h6 className="card-category">Designer</h6>
@@ -106,47 +98,31 @@ function Founders() {
                 <Button
                   className="btn-just-icon btn-neutral"
                   color="link"
-                  href="#pablo"
+                  href="https://twitter.com/zero"
                   onClick={(e) => e.preventDefault()}
                 >
                   <i className="fa fa-twitter" />
                 </Button>
-                <Button
-                  className="btn-just-icon btn-neutral ml-1"
-                  color="link"
-                  href="#pablo"
-                  onClick={(e) => e.preventDefault()}
-                >
-                  <i className="fa fa-google-plus" />
-                </Button>
-                <Button
-                  className="btn-just-icon btn-neutral ml-1"
-                  color="link"
-                  href="#pablo"
-                  onClick={(e) => e.preventDefault()}
-                >
-                  <i className="fa fa-linkedin" />
-                </Button>
               </CardFooter>
             </Card>
           </Col>
-          <Col md="4">
+          <Col md="3">
             <Card className="card-profile card-plain">
               <div className="card-avatar">
-                <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                <a href="#dereek69" onClick={(e) => e.preventDefault()}>
                   <img
-                    alt="..."
+                    alt="dereek69 profile image"
                     src={
-                      require("assets/img/faces/erik-lucatero-2.jpg")
+                      require("assets/img/faces/dereek69-profile-image.jpg")
                         .default
                     }
                   />
                 </a>
               </div>
               <CardBody>
-                <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                <a href="#dereek69" onClick={(e) => e.preventDefault()}>
                   <div className="author">
-                    <CardTitle tag="h4">Robert Orben</CardTitle>
+                    <CardTitle tag="h4">Dereek69</CardTitle>
                     <h6 className="card-category">Developer</h6>
                   </div>
                 </a>
@@ -161,26 +137,49 @@ function Founders() {
                 <Button
                   className="btn-just-icon btn-neutral"
                   color="link"
-                  href="#pablo"
+                  href="https://twitter.com/Dereek70"
                   onClick={(e) => e.preventDefault()}
                 >
                   <i className="fa fa-twitter" />
                 </Button>
+              </CardFooter>
+            </Card>
+          </Col>
+          <Col md="3">
+            <Card className="card-profile card-plain">
+              <div className="card-avatar">
+                <a href="#munair" onClick={(e) => e.preventDefault()}>
+                  <img
+                    alt="munair profile image"
+                    src={
+                      require("assets/img/faces/munair-profile-image.jpg")
+                        .default
+                    }
+                  />
+                </a>
+              </div>
+              <CardBody>
+                <a href="#munair" onClick={(e) => e.preventDefault()}>
+                  <div className="author">
+                    <CardTitle tag="h4">Munair</CardTitle>
+                    <h6 className="card-category">Developer</h6>
+                  </div>
+                </a>
+                <p className="card-description text-center">
+                  The strength of the team is each individual member. The
+                  strength of each member is the team. If you can laugh
+                  together, you can work together, silence isn’t golden,
+                  it’s deadly.
+                </p>
+              </CardBody>
+              <CardFooter className="text-center">
                 <Button
-                  className="btn-just-icon btn-neutral ml-1"
+                  className="btn-just-icon btn-neutral"
                   color="link"
-                  href="#pablo"
+                  href="https://twitter.com/munair"
                   onClick={(e) => e.preventDefault()}
                 >
-                  <i className="fa fa-google-plus" />
-                </Button>
-                <Button
-                  className="btn-just-icon btn-neutral ml-1"
-                  color="link"
-                  href="#pablo"
-                  onClick={(e) => e.preventDefault()}
-                >
-                  <i className="fa fa-linkedin" />
+                  <i className="fa fa-twitter" />
                 </Button>
               </CardFooter>
             </Card>
