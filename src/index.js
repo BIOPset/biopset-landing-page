453e42f1357302d70a92b627c028165234582794
munair <munair@gmail.com>
2021-09-12 19:57:37 +0900

    add react-router-dom (redux?)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// import react router
+import { BrowserRouter } from 'react-router-dom'
+
 // styles
 import "bootstrap/scss/bootstrap.scss";
 import "assets/scss/paper-kit.scss?v=1.3.0";
 import "assets/demo/demo.css?v=1.3.0";
 
 // pages
-import LandingPage from "views/LandingPage.js";
 
 // react
-ReactDOM.render( <LandingPage />, document.getElementById('root') );
+const App = () => (
+  <BrowserRouter>
+    <h1>Hello World</h1>
+  </BrowserRouter>
+);
+
+ReactDOM.render( <App />, document.getElementById('root') );
